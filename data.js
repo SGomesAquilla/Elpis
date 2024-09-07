3c9950f610ceb9f94edb8a875b35bf4c11855e75
Áquilla Siqueira Gomes <sgomesaquilla@gmail.com>
2024-09-06 21:46:39 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,55 @@
+let dataSongs = [
+    {
+        name: "Amazing Grace",
+        autor: "Hayley Westenra",
+        lyrics: "Amazing grace, how sweet the sound That saved a wretch like me I once was lost, but now I'm found Was blind, but now I see 'Twas grace that taught my heart to fear"
+    },
+    {
+        name: "In The River",
+        autor: "Jesus Culture",
+        lyrics: "There is a river where goodness flows There is a fountain that drowns sorrows There is an ocean deeper than fear The tide is rising, rising There is a current stirring deep inside"
+    },
+    {
+        name: "Glorioso Amor",
+        autor: "Nova Igreja Music",
+        lyrics: "Dos meus erros você se esqueceu Dos meus pecados não se lembra mais Eu não consigo compreender tão grande amo Você me cobre com tua compaixão Tua graça vem e enche o meu ser Eu só consigo me render a ti Senhor"
+    },
+    {
+        name: "Oceans",
+        autor: "Hillsong United",
+        lyrics: "You call me out upon the waters The great unknown where feet may fail And there I find You in the mystery In oceans deep my faith will stand And I will call upon Your Name And keep my eyes above the waves When oceans rise My soul will rest in Your embrace For I am Yours and You are mine"
+    },
+    {
+        name: "Awake My Soul",
+        autor: "Hillsong Worship",
+        lyrics: "There is a sound I love to hear It’s the sound of the Saviour’s robe As He walks into the room where people pray Where we hear praises He hears faith There is a sound I love to hear It’s the sound of the Saviour’s robe As He walks into the room where people pray Where we hear worship He hears faith"
+    },
+]
+
+let dataReading = [
+    {
+        title: "Daily Devotion",
+        theme: "Don’t Quit",
+        summary: "And let us not lose heart and grow weary and faint in acting nobly and doing right, for in due time and at the appointed season we shall reap, if we do not loosen and relax our courage and faint."
+    },
+    {
+        title: "Generosity",
+        theme: "Generosity",
+        summary: "God is a generous god, but throughout the Bible, humans hoard God’s good gifts. In this video, we look at how to live by God’s generous example"
+    },
+    {
+        title: "Son of Man",
+        theme: "Jesus Christ",
+        summary: "Jesus often referred to himself as the Son of Man. But what does this mean? Explore this fascinating phrase to see its significance."
+    },
+    {
+        title: "Knowing the Bible: Romans",
+        theme: "Book of Romans",
+        summary: "The apostle Paul’s letter to the Romans is the longest of his letters and is brimming with his exhilarating captivation with the gospel of Jesus Christ."
+    },
+    {
+        title: "Inspirational word",
+        theme: "Encouragements and Faith",
+        summary: "For we walk by faith, not by sight."
+    },
+]
