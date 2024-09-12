0df9321b5065b1f923b5319cc1d4abb404ff82ea
Áquilla Siqueira Gomes <sgomesaquilla@gmail.com>
2024-09-12 12:09:26 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,31 @@
 const dataSongs = [
     {
-        title: "Amazing Grace",
-        autor: "Hayley Westenra",
-        lyrics: "Amazing grace, how sweet the sound That saved a wretch like me I once was lost, but now I'm found Was blind, but now I see 'Twas grace that taught my heart to fear"
+        title: "Broken Vessels (Amazing Grace)",
+        link: "https://open.spotify.com/intl-pt/track/35WtFQK3iG2WPjstuuKaVc?si=0b125e50fa794e7c",
+        autor: "Hillsong Worship",
+        lyrics: "Amazing grace, how sweet the sound That saved a wretch like me I once was lost, but now I'm found Was blind, but now I see. Oh I can see you now"
     },
     {
         title: "In The River",
+        link: "https://open.spotify.com/intl-pt/track/2SAyn3QgskvTpUAaFPZ0Hn",
         autor: "Jesus Culture",
         lyrics: "There is a river where goodness flows There is a fountain that drowns sorrows There is an ocean deeper than fear The tide is rising, rising There is a current stirring deep inside"
     },
     {
         title: "Glorioso Amor",
+        link: "https://open.spotify.com/intl-pt/track/0FhYIxuJUqsiljzY4zpuCH",
         autor: "Nova Igreja Music",
         lyrics: "Dos meus erros você se esqueceu Dos meus pecados não se lembra mais Eu não consigo compreender tão grande amo Você me cobre com tua compaixão Tua graça vem e enche o meu ser Eu só consigo me render a ti Senhor"
     },
     {
         title: "Oceans",
+        link: "https://open.spotify.com/intl-pt/track/5Mw9bXG1dLNhbjofkVS2oR",
         autor: "Hillsong United",
         lyrics: "You call me out upon the waters The great unknown where feet may fail And there I find You in the mystery In oceans deep my faith will stand And I will call upon Your Name And keep my eyes above the waves When oceans rise My soul will rest in Your embrace For I am Yours and You are mine"
     },
     {
         title: "Awake My Soul",
+        link: "https://open.spotify.com/intl-pt/track/6AvQ48ohekN6f2eqZCYICX",
         autor: "Hillsong Worship",
         lyrics: "There is a sound I love to hear It’s the sound of the Saviour’s robe As He walks into the room where people pray Where we hear praises He hears faith There is a sound I love to hear It’s the sound of the Saviour’s robe As He walks into the room where people pray Where we hear worship He hears faith"
     },
@@ -29,26 +34,31 @@ const dataSongs = [
 const dataReading = [
     {
         title: "Daily Devotion",
+        link: "https://joycemeyer.org/DailyDevo/2024/09/06-Dont-Quit",
         theme: "Don’t Quit",
         summary: "And let us not lose heart and grow weary and faint in acting nobly and doing right, for in due time and at the appointed season we shall reap, if we do not loosen and relax our courage and faint."
     },
     {
         title: "Generosity",
+        link: "https://bibleproject.com/explore/video/generosity/",
         theme: "Generosity",
         summary: "God is a generous god, but throughout the Bible, humans hoard God’s good gifts. In this video, we look at how to live by God’s generous example"
     },
     {
         title: "Son of Man",
+        link: "https://bibleproject.com/explore/video/son-of-man/",
         theme: "Jesus Christ",
         summary: "Jesus often referred to himself as the Son of Man. But what does this mean? Explore this fascinating phrase to see its significance."
     },
     {
         title: "Knowing the Bible: Romans",
+        link: "https://www.thegospelcoalition.org/course/knowing-bible-romans/#week-1-overview",
         theme: "Book of Romans",
         summary: "The apostle Paul’s letter to the Romans is the longest of his letters and is brimming with his exhilarating captivation with the gospel of Jesus Christ."
     },
     {
         title: "Inspirational word",
+        link: "https://www.christianity.com/wiki/christian-life/how-do-we-walk-by-faith-and-not-by-sight.html",
         theme: "Encouragements and Faith",
         summary: "For we walk by faith, not by sight."
     },
